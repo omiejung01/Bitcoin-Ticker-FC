2f4b8d99e8ef79558cf0fe172296c7fcf96883b4
Dhirachat Chayaporn <omiejung@gmail.com>
2025-04-20 13:40:25 +0700

    My version of Bitcoin-Ticker-flutter course - London brewery app - with readme file

--- bitcoin_ticker_flutter/lib/brain.dart
+++ bitcoin_ticker_flutter/lib/brain.dart
@@ -4,42 +4,9 @@ import 'dart:convert';
 
 import 'dart:async';
 import 'dart:io';
-/*
-Future<double> getPrice(String base, String quote) async {
-  double result = 0.0;
-
-  String url = "https://rest.coinapi.io/v1/exchangerate/$base/$quote?apikey=${my.coin_io}";
-  http.Response response = await http.get(Uri.parse(url));
-
-  if (response.statusCode == 200) {
-    String body = response.body;
-    var encodedData = jsonDecode(body);
-    result = encodedData['rate'];
-  }
-  return result;
-}*/
-/*
-Future<dynamic> getPrice(String base, String quote) async {
-
-  String url = "${my.coin_url}$base/$quote?apikey=${my.coin_io}";
-  print(url);
-  http.Response response = await http.get(Uri.parse(url));
-  print(response);
-
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    return null;
-  }
-}*/
 
 Future<dynamic> fetch(String base, String quote) async {
   String url = "${my.cypto_api_io_url}$base/$quote";
-  //print (url);
-  //String key = "${my.cypto_api_io_key}";
-  //const Map<String, String> header = {
-  //  'X-API-Key': "$key",
-  //};
 
   final response = await http.get(
 
